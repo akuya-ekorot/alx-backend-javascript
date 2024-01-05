c18a193b99f81120b64bfa8c5cebba96c5508c13
Akuya Ekorot <akuyaekorot@gmail.com>
2024-01-05 06:48:46 +0300

    add pretterrc

--- 0x00-ES6_basic/tests/0-constants.test.js
+++ 0x00-ES6_basic/tests/0-constants.test.js
@@ -1,7 +1,10 @@
-import { taskFirst, taskNext } from "../0-constants";
-import { test, expect } from "@jest/globals";
+import { taskFirst, taskNext } from '../0-constants';
+import { test, expect } from '@jest/globals';
 
-test("works with constants", () => {
-  expect(taskFirst()).toBe("I prefer const when I can.");
-  expect(taskNext()).toBe("But sometimes let is okay");
+test('works with constants', () => {
+  expect(taskFirst()).toBe('I prefer const when I can.');
+  expect(taskNext()).toBe('But sometimes let is okay');
+  expect(`${taskFirst()} ${taskNext()}`).toBe(
+    'I prefer const when I can. But sometimes let is okay',
+  );
 });
