d35e7d969e64a0956dc99d28d290b5368d001594
Akuya Ekorot <akuyaekorot@gmail.com>
2024-01-09 08:37:44 +0300

    get list of students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+/**
+ * Returns an array of student objects
+ * @typedef {{id: number, firstName: string, location: string}} Student
+ *
+ * @returns {Student[]} students
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
+  ];
+}
