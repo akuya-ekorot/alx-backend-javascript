3c909f561dbfc0af5268fb58307c0a09b8e6abdc
Akuya Ekorot <akuyaekorot@gmail.com>
2024-03-04 18:17:57 +0300

    return variables in order initiated

--- 0x00-ES6_basic/tests/1-block-scoped.test.js
+++ 0x00-ES6_basic/tests/1-block-scoped.test.js
@@ -1,7 +1,10 @@
 import taskBlock from '../1-block-scoped';
 import { test, expect } from '@jest/globals';
 
-test('taskBlock', () => {
+test('taskBlock with true', () => {
   expect(taskBlock(true)).toEqual([false, true]);
+});
+
+test('taskBlock with false', () => {
   expect(taskBlock(false)).toEqual([false, true]);
 });
