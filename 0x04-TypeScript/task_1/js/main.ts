a91e5fe2ecbb18c18aaaa1776003b885f294790c
Akuya Ekorot <akuyaekorot@gmail.com>
2024-03-13 12:08:15 +0300

    write a class
    Write a student class
    - [x] describe the class with an interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,34 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+interface Student {
+  workOnHomeWork: () => string;
+  displayName: () => string;
+}
+
+class StudentClass implements Student {
+  fistName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.fistName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomeWork() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.fistName;
+  }
+}
