458e01248516a9c1336bf65b4da042e4a306c466
Akuya Ekorot <akuyaekorot@gmail.com>
2024-03-27 10:34:02 +0300

    ClassRoom class
    Create a ClassRoom task
    Accepts one parameter `maxStudentSize`
    Has one attribute _maxStudentSize

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+export default class ClassRoom {
+  _maxStudentSize;
+
+  constructor(maxStudentSize) {
+    this._maxStudentSize = maxStudentSize;
+  }
+}
---
+++ 0x02-ES6_classes/tests/0-classroom.spec.js
@@ -0,0 +1,6 @@
+import ClassRoom from '../0-classroom';
+
+test('ClassRoom class', () => {
+  const classRoom = new ClassRoom(10);
+  expect(classRoom._maxStudentSize).toBe(10);
+});
